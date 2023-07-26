14192b8243edd05e6ecf8471d7a44138c099418e
Brett Hudson <Brett@brett-techrepair.com>
2023-07-26 19:55:46 -0400

    Security Updates
    -Updated ApiKey Location
    -Updated Privacy Policy Location https://sites.google.com/view/bretttechcoding/privacypolicycogop?authuser=1

---
+++ app/src/main/java/com/cogop/riverrougecogop/ApiKey.java
@@ -0,0 +1,5 @@
+package com.cogop.riverrougecogop;
+
+public class ApiKey {
+    public static final String MY_API_KEY = "AIzaSyDDfdxkhYxSJjyn2P2IQc3d-A8ATrusP4A";
+}
--- app/src/main/java/com/cogop/riverrougecogop/MyApp.java
+++ app/src/main/java/com/cogop/riverrougecogop/MyApp.java
@@ -7,14 +7,16 @@ import com.google.firebase.FirebaseOptions;
 
 public class MyApp extends Application {
 
+
     @Override
     public void onCreate() {
         super.onCreate();
+        String apiKey = ApiKey.MY_API_KEY;
 
         // Initialize Firebase with the correct database URL
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setApplicationId("com.cogop.riverrougecogop")
-                .setApiKey("AIzaSyDDfdxkhYxSJjyn2P2IQc3d-A8ATrusP4A")
+                .setApiKey(apiKey)
                 .setDatabaseUrl("https://riverrougecogop-bbe78-default-rtdb.firebaseio.com") // Replace with your database URL
                 .build();
 
