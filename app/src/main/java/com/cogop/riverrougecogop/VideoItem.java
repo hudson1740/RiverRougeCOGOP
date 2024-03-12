b7a4e26826a62fb70d3ccdd318f690014a5a8f00
Brett Hudson <Brett@brett-techrepair.com>
2024-03-12 09:54:22 -0400

    Youtube video list update
    not yet ready working on features for this

---
+++ app/src/main/java/com/cogop/riverrougecogop/VideoItem.java
@@ -0,0 +1,13 @@
+package com.cogop.riverrougecogop;
+
+public class VideoItem {
+    private String title;
+
+    public VideoItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
